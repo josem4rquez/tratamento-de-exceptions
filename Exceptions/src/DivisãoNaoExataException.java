5a4c14e6681816322992c5269ff0b26ab4c584da
josem4rquez <josebermarques@gmail.com>
2024-11-21 17:10:22 -0300

    Atualização

--- Exceptions/src/DivisãoNaoExataException.java
+++ Exceptions/src/DivisãoNaoExataException.java
@@ -1,9 +1,11 @@
 public class DivisãoNaoExataException extends Exception{
     
+    @SuppressWarnings("unused")
     private int numerador;
+    @SuppressWarnings("unused")
     private int denominador;
     
-    public DivisãoNaoExataException(String message, int numerado, int denominador) {
+    public DivisãoNaoExataException(String message, int numerador, int denominador) {
         super(message);
         this.numerador = numerador;
         this.denominador = denominador;
